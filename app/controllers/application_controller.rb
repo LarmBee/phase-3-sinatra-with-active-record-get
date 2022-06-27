cfc80e811042e1872f795b414a7a223863f35a15
Brandon <brandonkanute@gmail.com>
2022-06-28 01:27:22 +0300

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,23 @@
 class ApplicationController < Sinatra::Base
 
+  set :default_content_type, 'application/json'
   get '/' do
     { message: "Hello world" }.to_json
   end
 
+  get '/games' do
+    #get all games
+    games = Game.all
+    #return to json
+    games.to_json
+  end
+
+  get '/games/:id' do
+    game = Game.find(params[:id])
+    game.to_json(only: [:id, :title, :genre, :price],
+       include: {reviews: { only: [:comment, :score],
+       include: {user: { only: [:name] }
+      } }
+    })
+  end
 end
